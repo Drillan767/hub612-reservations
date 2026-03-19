78df6526a3ce6b1fbf01fd65e2aa5adaba07fc30
joseph.levarato <joseph@izypay.fr>
2026-03-19 22:52:57 +0100

    Added field limits and transaction history hook

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -9,6 +10,7 @@ import (
 	_ "hub612-reservations/migrations"
 
 	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -40,8 +42,105 @@ func main() {
 		return e.Next()
 	})
 
+	// Create transaction history entry
+	app.OnRecordCreateRequest("users").BindFunc(func(e *core.RecordRequestEvent) error {
+		return createCreditTransaction(app, e, "create")
+	})
+
+	app.OnRecordUpdateRequest("users").BindFunc(func(e *core.RecordRequestEvent) error {
+		return createCreditTransaction(app, e, "update")
+	})
+
+	app.OnRecordCreateRequest()
+
+	app.Cron().MustAdd("mensual-credits", "0 0 1 * *", func() {
+		// Load all users and their companies
+
+		users := []struct {
+			Id      string `db:"id"`
+			Credits int    `db:"credits"`
+			Company struct {
+				Id             string `db:"id"`
+				MensualCredits int    `db:"mensual_credits"`
+			} `db:"company"`
+		}{}
+
+		err := app.DB().
+			Select("users.*").
+			InnerJoin("companies", dbx.NewExp("companies.user_id = users.id")).
+			All(&users)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, u := range users {
+			user, err := app.FindRecordById("users", u.Id)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			user.Set("credits", u.Credits+u.Company.MensualCredits)
+
+			if err := app.Save(user); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	})
+
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func createCreditTransaction(app *pocketbase.PocketBase, e *core.RecordRequestEvent, action string) error {
+	oldCredits := 0
+
+	if action == "update" {
+		oldRecord, err := app.FindRecordById("users", e.Record.Id)
+		if err != nil {
+			return err
+		}
+
+		oldCredits = oldRecord.GetInt("credits")
+
+		if err := e.Next(); err != nil {
+			return err
+		}
+	}
+
+	newCredits := e.Record.GetInt("credits")
+
+	if oldCredits == newCredits {
+		return nil
+	}
+
+	info, err := e.RequestInfo()
+	if err != nil {
+		return err
+	}
+
+	collection, err := app.FindCollectionByNameOrId("credits_transaction")
+	if err != nil {
+		return err
+	}
+
+	// TODO: handle when credits is deducted post booking
+	// One solution could be to check if new amount is lower than current and if
+	// from and to are identical, but doesn't seem viable
+
+	transaction := core.NewRecord(collection)
+	transaction.Set("amount", newCredits)
+	transaction.Set("from", info.Auth.Id)
+	transaction.Set("to", e.Record.Id)
+
+	if err := app.Save(transaction); err != nil {
+		return err
+	}
+
+	return nil
 }
